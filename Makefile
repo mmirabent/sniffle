417fc2b39b5688491b2997260e4b72e71df2f961
Marcos Mirabent <tehbp@test.tehbp.net>
2015-08-07 00:58:30 -0400

    Found example sniffer code, borrowing some of that http://www.tcpdump.org/sniffex.c Also added part where the tcp handshake tracking takes place

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 main : main.c
-	gcc -o main -lpcap -pedantic-errors -Wall -Wextra main.c
+	gcc -o main -lpcap -pedantic -Wall -Wextra main.c
--- main.c
+++ main.c
@@ -1,16 +1,78 @@
 #include <pcap/pcap.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <netinet/in.h>
 
 void print_error(char* err);
+void print_pcap_err(pcap_t *p);
 void process_packet(u_char *user, const struct pcap_pkthdr *h, const u_char *bytes);
 
+/* default snap length (maximum bytes per packet to capture) */
+#define SNAP_LEN 1518
+
+/* ethernet headers are always exactly 14 bytes [1] */
+#define SIZE_ETHERNET 14
+
+/* Ethernet addresses are 6 bytes */
+#define ETHER_ADDR_LEN  6
+
+/* Ethernet header */
+struct sniff_ethernet {
+    u_char ether_dhost[ETHER_ADDR_LEN];
+    u_char ether_shost[ETHER_ADDR_LEN];
+    u_short ether_type;
+};
+
+struct sniff_ip {
+    u_char  ip_vhl;                 /* version << 4 | header length >> 2 */
+    u_char  ip_tos;                 /* type of service */
+    u_short ip_len;                 /* total length */
+    u_short ip_id;                  /* identification */
+    u_short ip_off;                 /* fragment offset field */
+    #define IP_RF 0x8000            /* reserved fragment flag */
+    #define IP_DF 0x4000            /* dont fragment flag */
+    #define IP_MF 0x2000            /* more fragments flag */
+    #define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
+    u_char  ip_ttl;                 /* time to live */
+    u_char  ip_p;                   /* protocol */
+    u_short ip_sum;                 /* checksum */
+    struct  in_addr ip_src,ip_dst;  /* source and dest address */
+};
+
+#define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip)                (((ip)->ip_vhl) >> 4)
+
+typedef u_int tcp_seq;
+struct sniff_tcp {
+    u_short th_sport;   /* Destination port */
+    u_short th_dport;   /* Source port */
+    tcp_seq th_seq;     /* Sequence number */
+    tcp_seq th_ack;     /* Acknowledgment number */
+    u_char  th_offx2;   /* data offset, rsvd */
+    #define TH_OFF(th) (((th)->th_offx2 & 0xf0) >> 4)
+    u_char  th_flags;
+    #define TH_FIN  0x01
+    #define TH_SYN  0x02
+    #define TH_RST  0x04
+    #define TH_PUSH 0x08
+    #define TH_ACK  0x10
+    #define TH_URG  0x20
+    #define TH_ECE  0x40
+    #define TH_CWR  0x80
+    #define TH_FLAGS    (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+    u_short th_win;     /* window */
+    u_short th_sum;     /* checksum */
+    u_short th_urp;     /* urgent pointer */
+};
+
 int main(int argc, char** argv) {
-    /* Error buffer used by many pcap functions to return error messages */
-    char errbuf[PCAP_ERRBUF_SIZE];
+
+/* Error buffer used by many pcap functions to return error messages */
+char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t *capture;
     char *dev;
     int ret;
+    struct bpf_program *filter;
 
     /* If no command line arguments given, lookup a device to open. Else use 
      * first argument */
@@ -40,24 +102,54 @@ int main(int argc, char** argv) {
        should be set before calling this */
     ret = pcap_activate(capture);
     if(ret)
-        print_error(pcap_geterr(capture));
+        print_pcap_err(capture);
 
-    printf("The pcap_activate return value was %d\n", ret);
+    /* Set up the packet filter and compile it down */
+    filter = malloc(sizeof(struct bpf_program));
+    ret = pcap_compile(capture, filter, "tcp", 1, PCAP_NETMASK_UNKNOWN);
+    if(ret)
+        print_pcap_err(capture);
+
+    pcap_setfilter(capture, filter);
+    if(ret)
+        print_pcap_err(capture);
 
     /* Start reading packets */
     ret = pcap_loop(capture, 0, process_packet, '\0');
-    printf("The pcap_loop return value was %d\n", ret);
     if(ret)
-        print_error(pcap_geterr(capture));
+        print_pcap_err(capture);
 
     return 0;
 }
 
-void process_packet(u_char *user, const struct pcap_pkthdr *h, const u_char *bytes) {
-    u_int i;
-    printf("Packet\n");
-    for(i = 0; i < h->caplen && i < h->len; i+=4)
-        printf("%02x%02x%02x%02x\n", bytes[i], bytes[i+1], bytes[i+2], bytes[i+3]);
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+void process_packet(u_char *user, const struct pcap_pkthdr *h, const u_char *packet) {
+    /* declare the ip and tcp structs that will allow us easy access to the data
+     * later */
+    const struct sniff_ip *ip;
+    const struct sniff_tcp *tcp;
+    int size_ip;
+    int size_tcp;
+
+    /* By asigning the correct memory address to the ip and tcp structs, we can
+     * use the structs to get at the important information in the packet headers */
+    ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
+    size_ip = IP_HL(ip)*4;
+    if(size_ip < 20) return; /* Invalid IP header */
+
+    tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
+    size_tcp = TH_OFF(tcp)*4;
+    if(size_tcp < 20) return; /* Invalid tcp header */
+
+    /* If SYN, add to syn table */
+    /* If SYN-ACK, match with syn table entry, create delta, ship off for 
+     * processing elsewhere(file IO for now). Then create entry in synack table*/
+    /* If ACK, attempt to match with synack table entry, create delta, ship off for processing */
+
+    /* syn table should have source ip, dest ip, source port, dest port, and syn-number */
+    /* syn-ack table should have source ip, dest ip, source port, dest port, and syn-number */
+    /* match by comparing ack-number+1 to syn number in table */
+
 }
 
 void print_error(char* err) {
@@ -65,3 +157,6 @@ void print_error(char* err) {
     exit(1);
 }
 
+void print_pcap_err(pcap_t *p) {
+    print_error(pcap_geterr(p));
+}
