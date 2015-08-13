f2a07dc757f864fabf978d9a2398ebbc7e8b7558
Pizzorni <giorgio@pizzorni.com>
2015-08-13 18:48:17 -0400

    added function signatures to output.h

--- output.h
+++ output.h
@@ -5,5 +5,7 @@
 #include <arpa/inet.h>
 
 void report_server_rtt(struct in_addr client, struct in_addr server, u_short sport, u_short dport, int rtt);
+void reverse_dns_lookup(char * ip_addr, char * buffer);
+int exec_cmd(char * cmd, char ** args, char * buffer);
 
 #endif
