791bc7c7254745d2610c8b6bc85c26b6fc4ee69b
Pizzorni <giorgio@pizzorni.com>
2015-08-13 02:20:56 -0400

    pulled master branch

--- output.c
+++ output.c
@@ -5,6 +5,6 @@
  * changed to print to a file.
  */
 void report_server_rtt(struct in_addr client, struct in_addr server, u_short sport, u_short dport, int rtt) {
-    printf("%s:%d -> ", inet_ntoa(client), sport);
-    printf("%s:%d %dms\n", inet_ntoa(server), dport, rtt);
+    printf("%sa:%d -> ", inet_ntoa(client), sport);
+    printf("%sb:%d %dms\n", inet_ntoa(server), dport, rtt);
 }
