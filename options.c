853d6cfbd5cdabf4b23f28faedca1300af0bb466
Marcos Mirabent <mmirabent@gmail.com>
2015-08-18 00:29:39 -0400

    Fixed typo in usage

--- options.c
+++ options.c
@@ -80,7 +80,7 @@ void process_options(int argc, char** argv) {
                 printf("-l live capture\n");
                 printf("-f packet capture\n");
                 printf("-o csv output\n");
-                printf("-n reverse-dns");
+                printf("-n reverse-dns\n");
                 printf("-s number of half open connections tracked\n");
                 printf("-h help and usage\n");
                 exit(0);
