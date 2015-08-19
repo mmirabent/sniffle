68609b85ad2f8de264d1884775c51c932d4e69d9
Marcos Mirabent <marcos@mirabent.net>
2015-08-18 20:34:19 -0400

    Added test.sh and makefile target for testing

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
 main : main.c decode.h session.h session.c output.c output.h options.c options.h
 	clang -o sniffle -std=gnu90 -lpcap -g -pedantic -Weverything main.c session.c output.c options.c
 
+test : main
+	sudo ./test.sh
+
+.PHONY : test
+
+
--- test.sh
+++ test.sh
@@ -1,6 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+
+# Very crude test script. Runs the sniffle program in live capture more and 
+# uses a python script to generate a bunch of TCP connections
+
+# Check for root, die if not
+if [ $UID -ne 0 ];
+then
+    echo Must be root;
+    exit;
+fi
 
 # Test the sniffle program
+./sniffle &
+SNIFFLE_PID=$!
 
+# Generate TCP connections
+python generate_connections.py
 
+kill -9  $SNIFFLE_PID
 
